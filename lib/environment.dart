6a36061bc2a0a6fc251a76e681f6fa3b25ddbdcf
abondarew <abondarew@gmail.com>
2021-02-03 17:53:45 +0200

    Initial

--- lib/environment.dart
+++ lib/environment.dart
@@ -1,5 +1,11 @@
 class Environment {
   static bool debug = false;
+  static DataBaseType dataBaseType = DataBaseType.SQLITE;
 
-  static void init({bool debug}) => Environment.debug = debug;
+  static void init({bool debug, DataBaseType dataBaseType}) {
+    Environment.debug = debug;
+    Environment.dataBaseType = dataBaseType;
+  }
 }
+
+enum DataBaseType { SQLITE, MEMORY }
