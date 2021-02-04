9d69f1cbb56077e579a1860d2b5f9578901a8081
andy <abondarew@gmail.com>
2021-02-05 00:28:14 +0200

    Initial

--- lib/app/core/common/model/domain/dashboard_widget_model/account_amount.dart
+++ lib/app/core/common/model/domain/dashboard_widget_model/account_amount.dart
@@ -1,9 +1,5 @@
 import 'package:mymoneyorganizer/app/core/common/model/domain/dashboard_widget_model_contract.dart';
 
-class AccountAmountModel implements DashboardWidgetModelContract{
+class AccountAmountModel implements DashboardWidgetModelContract {}
 
-}
-
-class AccountAmountSettingModel{
-
-}
+class AccountAmountSettingModel {}
