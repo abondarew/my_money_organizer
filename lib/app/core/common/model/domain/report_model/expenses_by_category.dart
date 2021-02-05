ad1a5f3046837cfcb07a51305c58b68a5791e1f2
andy <abondarew@gmail.com>
2021-02-05 18:17:48 +0200

    Initial

--- lib/app/core/common/model/domain/report_model/expenses_by_category.dart
+++ lib/app/core/common/model/domain/report_model/expenses_by_category.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/foundation.dart';
-import 'package:mymoneyorganizer/app/core/common/constant/monetary_movement.dart';
-import 'package:mymoneyorganizer/app/core/common/constant/report_period.dart';
 import 'package:mymoneyorganizer/app/core/common/model/domain/report_model/contract/dashboard_widget_model_contract.dart';
 import 'package:mymoneyorganizer/app/core/common/model/read/category_model.dart';
+import 'package:mymoneyorganizer/app/core/common/util/monetary_movement.dart';
+import 'package:mymoneyorganizer/app/core/common/util/report_period.dart';
 import 'package:mymoneyorganizer/app/core/dashboard_widgets/dashboard_widget_contact.dart';
 
 class ExpensesByCategoryModel implements DashboardWidgetContract {
@@ -18,7 +18,7 @@ class ExpensesByCategoryModel implements DashboardWidgetContract {
 /// Формируются, исходя из переданного типа отчетного периода [ReportingPeriodType]
 */
 class ExpensesByCategorySettingModel implements DashboardWidgetSettingModelContract {
-  DateTime startDate;
+  final DateTime startDate;
   DateTime endDate;
   List<CategoryReadModel> includingCategory;
   List<MonetaryMovementType> includingMonetaryMovement;
@@ -28,39 +28,8 @@ class ExpensesByCategorySettingModel implements DashboardWidgetSettingModelContr
       {@required ReportingPeriodType periodType,
       this.includingCategory,
       this.includingMonetaryMovement,
-      this.excludingMonetaryMovement}) {
-    DateTime currentDate = new DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day);
-    switch (periodType) {
-      case ReportingPeriodType.day:
-        {
-          startDate = endDate = currentDate;
-        }
-        break;
-      case ReportingPeriodType.week:
-        {
-          startDate = new DateTime(currentDate.year, currentDate.month, currentDate.day);
-          startDate = (startDate.subtract(Duration(days: startDate.weekday - 1)));
-          endDate = currentDate;
-        }
-        break;
-      case ReportingPeriodType.month:
-        {
-          startDate = new DateTime(currentDate.year, currentDate.month, 1);
-          endDate = currentDate;
-        }
-        break;
-      case ReportingPeriodType.year:
-        {
-          startDate = new DateTime(currentDate.year, 1, 1);
-          endDate = currentDate;
-        }
-        break;
-      case ReportingPeriodType.seven_days:
-        {
-          startDate = new DateTime(currentDate.year, currentDate.month, currentDate.day);
-          startDate = startDate.subtract(Duration(days: 7));
-          endDate = currentDate;
-        }
-    }
-  }
+      this.excludingMonetaryMovement})
+      :
+        startDate = getPeriodReport(periodType)["startDate"],
+        endDate = getPeriodReport(periodType)["endDate"] {}
 }
---
+++ lib/app/core/common/util/report_period.dart
@@ -0,0 +1,50 @@
+enum ReportingPeriodType {
+  day, //текущий день
+  week, //текущая неделя
+  seven_days, //последние 7 дней
+  month, //месяц
+  three_months, //3 месяца (квартал)
+  year, //год
+  specified, //явно указанный
+  all //весь период
+}
+
+Map<String, DateTime> getPeriodReport(ReportingPeriodType periodType) {
+  Map<String, DateTime> result = new Map();
+  DateTime currentDate = new DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day);
+  switch (periodType) {
+    case ReportingPeriodType.day:
+      {
+        result["startDate"] = currentDate;
+        result["endDate"] = currentDate;
+        return result;
+      }
+    case ReportingPeriodType.week:
+      {
+        result["startDate"] = new DateTime(currentDate.year, currentDate.month, currentDate.day)
+            .subtract(Duration(days: currentDate.weekday - 1));
+        result["endDate"] = currentDate;
+        return result;
+      }
+    case ReportingPeriodType.month:
+      {
+        result["startDate"] = new DateTime(currentDate.year, currentDate.month, 1);
+        result["endDate"] = currentDate;
+        return result;
+      }
+    case ReportingPeriodType.year:
+      {
+        result["startDate"] = new DateTime(currentDate.year, 1, 1);
+        result["endDate"] = currentDate;
+        return result;
+      }
+    case ReportingPeriodType.seven_days:
+      {
+        result["startDate"] =
+            new DateTime(currentDate.year, currentDate.month, currentDate.day).subtract(Duration(days: 7));
+        result["endDate"] = currentDate;
+        return result;
+      }
+  }
+  return result;
+}
