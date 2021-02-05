440a20fd9ee37e82eaa78ac52325bb22f6f2f6ff
andy <abondarew@gmail.com>
2021-02-05 09:17:58 +0200

    Initial

--- lib/app/core/account/command/dispather/command_dispather.dart
+++ lib/app/core/account/command/dispather/command_dispather.dart
@@ -1,16 +1,46 @@
+ /*Диспетчер команд для счетов. Держит хендлеры на создание, обновление, изменение и удаление счета,
+ / инициирует вызов execute хендлера
+ */
 import 'package:flutter/foundation.dart';
 import 'package:mymoneyorganizer/app/core/account/command/create_command.dart';
 import 'package:mymoneyorganizer/app/core/account/command/delete_command.dart';
 import 'package:mymoneyorganizer/app/core/account/command/open_close_account_command.dart';
+import 'package:mymoneyorganizer/app/core/common/base/command/base_command.dart';
 import 'package:mymoneyorganizer/app/core/common/base/command/base_handler.dart';
+import 'package:mymoneyorganizer/app/core/common/base/command/exceptions.dart';
 
 class AccountCommandDispatcher {
   final BaseCommandHandler<AccountCreateCommand> createCommandHandler;
   final BaseCommandHandler<AccountDeleteCommand> deleteCommandHandler;
   final BaseCommandHandler<AccountUpdateSignOfActivity> updateSignOfActivityHandler;
 
+  /* Конструктор принимает все доступные хендлеры
+
+  */
   const AccountCommandDispatcher(
       {@required this.createCommandHandler,
       @required this.deleteCommandHandler,
       @required this.updateSignOfActivityHandler});
+
+   /*Метод диспетчера: в зависимости от переданной команды отправляет ее (команду) в нужный хендлер
+
+   */
+  Future<void> dispatch(BaseCommand command){
+    bool commandIsExecuting = false;
+    if (command is AccountCreateCommand){
+      commandIsExecuting = true;
+      createCommandHandler.execute(command);
+    }
+    if (command is AccountDeleteCommand){
+      commandIsExecuting = true;
+      deleteCommandHandler.execute(command);
+    }
+    if (command is AccountUpdateSignOfActivity){
+      commandIsExecuting = true;
+      updateSignOfActivityHandler.execute(command);
+    }
+    if (!commandIsExecuting){
+      throw InvalidCommandException("command not identified");
+    }
+  }
 }
--- lib/app/core/common/base/command/exceptions.dart
+++ lib/app/core/common/base/command/exceptions.dart
@@ -1,1 +1,15 @@
 class BaseCommandException implements Exception {}
+
+class InvalidCommandException implements BaseCommandException{
+  final String message;
+
+  const InvalidCommandException ([this.message]);
+
+  @override
+  String toString() {
+    if (this.message == null){
+      return "InvalidCommandException";
+    }
+    return "InvalidCommandException: "+this.message;
+  }
+}
