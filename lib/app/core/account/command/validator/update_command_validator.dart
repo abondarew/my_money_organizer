4cc9cae0b6668fc1e2365bd67567fcd65ee2798c
andy <abondarew@gmail.com>
2021-02-11 12:26:44 +0200

    Initial

---
+++ lib/app/core/account/command/validator/update_command_validator.dart
@@ -0,0 +1,8 @@
+import 'package:mymoneyorganizer/app/core/common/base/command/base_validator.dart';
+
+class UpdateAccountCommandValidator extends BaseCommandValidator<AccountUpdateCommand>{
+  
+}
+
+class AccountUpdateCommand {
+}
