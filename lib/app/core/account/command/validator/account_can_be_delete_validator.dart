5a35c7619524e8b445d877fe68646adc1b4dabe3
abondarew <abondarew@gmail.com>
2021-02-12 08:16:38 +0200

    Initial

---
+++ lib/app/core/account/command/validator/account_can_be_delete_validator.dart
@@ -0,0 +1,9 @@
+import 'package:mymoneyorganizer/app/core/common/validator/base_validator.dart';
+
+class AccountCanBeDeleteValidator extends BaseValidator{
+  @override
+  Future<void> validate(value) async {
+    bool accountIsUse = await
+  }
+  
+}
--- lib/app/core/account/command/validator/delete_command_validator.dart
+++ lib/app/core/account/command/validator/delete_command_validator.dart
@@ -1,10 +1,15 @@
 import 'package:mymoneyorganizer/app/core/account/command/delete_command.dart';
+import 'package:mymoneyorganizer/app/core/account/command/validator/account_can_be_delete_validator.dart';
 import 'package:mymoneyorganizer/app/core/common/base/command/base_command.dart';
 import 'package:mymoneyorganizer/app/core/common/base/command/base_validator.dart';
+import 'package:mymoneyorganizer/app/core/common/validator/container.dart';
+import 'package:mymoneyorganizer/app/core/common/validator/list.dart';
 
 class DeleteAccountCommandValidator extends BaseCommandValidator<AccountDeleteCommand> {
   @override
   Future<void> validate(AccountDeleteCommand command) {
+    ContainerValidator containerValidator = new ContainerValidator();
+    ListValidator notUse = ListValidator([AccountCanBeDeleteValidator()]);
     // TODO: implement validate
     throw UnimplementedError();
   }
