270f03c9ce3c97aad36c00b3a7cb2304be177892
andy <abondarew@gmail.com>
2021-12-14 11:38:17 +0200

    fix buildTextToConfirmDelete

--- lib/app/view/common/util/generated_confirm_text_4_deleted.dart
+++ lib/app/view/common/util/generated_confirm_text_4_deleted.dart
@@ -1,11 +1,17 @@
 import 'package:mymoneyorganizer/app/lib/localization/utils.dart';
 
 String buildTextToConfirmDelete({List<String>? confirmData, String what = '', int viewEntities = 3}) {
-  if (confirmData == null || !(confirmData is List<String>)) return '';
+  if (confirmData == null) return '';
   if (confirmData.length <= viewEntities) {
-    return t('confirm_text', args: {'what': what}); //TODO, ('${what.toLowerCase()}:', confirmData.join(', ')));
+    return t('common.confirm.text', args: {
+      'what': what.toLowerCase(),
+      'records': confirmData.join(', '),
+    });
   } else {
-    return t(
-        'confirm_text_more'); //TODO ('${what.toLowerCase()}:', confirmData.take(viewEntities).join(', '), confirmData.length - viewEntities);
+    return t('common.confirm.text.more', args: {
+      'what': what.toLowerCase(),
+      'records': confirmData.take(viewEntities),
+      'quantity': confirmData.length - viewEntities,
+    });
   }
 }
